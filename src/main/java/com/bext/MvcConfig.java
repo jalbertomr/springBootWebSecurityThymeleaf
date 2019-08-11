37e69250a842893dbe0200ac75a4a7d4fd4a494f
bext <jalbertomr@yahoo.com>
2019-08-10 21:05:48 -0500

    Se agrega Security

--- src/main/java/com/bext/MvcConfig.java
+++ src/main/java/com/bext/MvcConfig.java
@@ -12,5 +12,6 @@ public class MvcConfig implements WebMvcConfigurer{
 		registry.addViewController("/home").setViewName("home");
 		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/paso2").setViewName("paso2");
+		registry.addViewController("/login").setViewName("login");
 	}
 }
